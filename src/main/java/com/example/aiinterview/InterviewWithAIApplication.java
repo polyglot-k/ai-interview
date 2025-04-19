4b0c37c67a753c77e0ca50c806323ea0715766d2
KNU-K <knukang334@gmail.com>
2025-04-20 03:04:49 +0900

    refactor: update import path for BackendAssistant and clean up commented code in InterviewWithAIApplication

--- src/main/java/com/example/aiinterview/InterviewWithAIApplication.java
+++ src/main/java/com/example/aiinterview/InterviewWithAIApplication.java
@@ -1,6 +1,6 @@
 package com.example.aiinterview;
 
-import com.example.aiinterview.llm.assistant.BackendAssistant;
+import com.example.aiinterview.module.llm.assistant.BackendAssistant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,26 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @RequiredArgsConstructor
 public class InterviewWithAIApplication implements CommandLineRunner{
-//    private  final Assistantaa backendAssistant;
-    private final BackendAssistant backendAssistant;
     public static void main(String[] args) {
         SpringApplication.run(InterviewWithAIApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-//        backendAssistant.chat(1,"너는 누구야?").onPartialResponse(System.out::println)
-//                .onCompleteResponse(System.out::println)
-//                .onError(Throwable::printStackTrace)
-//                .start();
-//        backendAssistant.chat(1,"내 이름이 뭐야?").onPartialResponse(System.out::println)
-//                .onCompleteResponse(System.out::println)
-//                .onError(Throwable::printStackTrace)
-//                .start();
-//
-//        backendAssistant.chat(2,"내 이름이 뭐야?").onPartialResponse(System.out::println)
-//                .onCompleteResponse(System.out::println)
-//                .onError(Throwable::printStackTrace)
-//                .start();
     }
 }
+
+
+
