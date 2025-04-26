f26f9abf31e7f2d17ff333b3807a01d05d34781d
KNU-K <knukang334@gmail.com>
2025-04-26 17:27:18 +0900

    fix: update InterviewRoomApplicationService to use InterviewSession and InterviewStreamer

--- src/main/java/com/example/aiinterview/module/interview/application/InterviewRoomApplicationService.java
+++ src/main/java/com/example/aiinterview/module/interview/application/InterviewRoomApplicationService.java
@@ -1,12 +1,13 @@
 package com.example.aiinterview.module.interview.application;
 
-import com.example.aiinterview.module.interview.domain.service.InterviewRoomDomainService;
+import com.example.aiinterview.global.redis.RedisService;
 import com.example.aiinterview.module.interview.domain.entity.InterviewMessage;
-import com.example.aiinterview.module.interview.domain.entity.InterviewRoom;
+import com.example.aiinterview.module.interview.domain.entity.InterviewSession;
+import com.example.aiinterview.module.interview.domain.entity.InterviewSessionStatus;
 import com.example.aiinterview.module.interview.infrastructure.repository.InterviewMessageRepository;
 import com.example.aiinterview.module.interview.infrastructure.repository.InterviewRoomRepository;
-import com.example.aiinterview.module.llm.LLMStreamer;
-import com.example.aiinterview.module.llm.prompt.LLMPromptType;
+import com.example.aiinterview.module.interviewer.InterviewStreamer;
+import com.example.aiinterview.module.interviewer.prompt.LLMPromptType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,17 +15,15 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
-import java.util.ArrayList;
 import java.util.List;
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class InterviewRoomApplicationService {
-    private final InterviewRoomDomainService roomDomainService;
-    private final LLMStreamer llmStreamer;
+    private final InterviewStreamer interviewStreamer;
     private final InterviewRoomRepository roomRepository;
     private final InterviewMessageRepository messageRepository;
-
+    private final RedisService redisService;
     // ==========================
     // 1. Public Methods
     // ==========================
@@ -32,25 +31,36 @@ public class InterviewRoomApplicationService {
     /**
      * 면접방 생성
      */
-    public Mono<InterviewRoom> createRoom() {
-        InterviewRoom room = InterviewRoom.create();
+    public Mono<InterviewSession> createRoom(Long memberId) {
+        InterviewSession room = InterviewSession.create(memberId);
         return roomRepository.save(room);
     }
 
     /**
+     * 메시지 가져오기
+     */
+    public Mono<List<InterviewMessage>> retrieveMessage(Long roomId, Long memberId){
+        return messageRepository.findByRoomId(roomId)
+                .collectList()
+                .switchIfEmpty(Mono.error(new IllegalArgumentException("면접방이 존재하지 않습니다.")));
+    }
+    /**
      * 메시지 추가
      */
     public Flux<String> sendMessage(Long roomId, Long memberId, String message) {
-        return  roomRepository.findById(roomId)
+        return roomRepository.findById(roomId)
                 .switchIfEmpty(Mono.error(new IllegalArgumentException("면접방이 존재하지 않습니다.")))
                 .flatMap(room -> {
-                    if (!room.getMemberId().equals(memberId)) {
+                    if (!room.getIntervieweeId().equals(memberId)) {
                         return Mono.error(new IllegalAccessException("접근 권한이 없습니다."));
+                    }
+                    if (InterviewSessionStatus.ENDED.equals(room.getStatus())) {
+                        return Mono.error(new IllegalArgumentException("이미 종료 된 면접 입니다."));
                     }
                     return Mono.just(room);
                 })
                 .flatMapMany(room -> saveHumanMessage(roomId, message)
-                        .flatMapMany(savedHumanMessage -> startLLMStreaming(roomId, message)));
+                        .flatMapMany(savedHumanMessage -> startLLMStreaming(roomId,message)));
     }
 
     /**
@@ -59,7 +69,7 @@ public class InterviewRoomApplicationService {
     public Mono<Void> completeInterview(Long roomId, String feedback) {
         return roomRepository.findById(roomId)
                 .flatMap(room -> {
-                    roomDomainService.completeInterview(room);
+//                    roomDomainService.completeInterview(room);
                     return roomRepository.save(room).then();
                 })
                 .switchIfEmpty(Mono.error(new IllegalArgumentException("면접방이 존재하지 않습니다.")));
@@ -69,24 +79,47 @@ public class InterviewRoomApplicationService {
     // 2. Private Helper Methods
     // ==========================
     private Flux<String> startLLMStreaming(Long roomId, String message) {
-        List<String> responseBuffer = new ArrayList<>();
-        return llmStreamer.stream(roomId.intValue(), LLMPromptType.BACKEND, message)
-                .doOnNext(responseBuffer::add)
-                .delayElements(java.time.Duration.ofMillis(300))
-                .doOnComplete(() -> saveLLMResponse(roomId, responseBuffer))
+        return interviewStreamer.stream(roomId.intValue(), LLMPromptType.BACKEND, message)
+                .doOnNext(partialResponse -> appendLLMResponsePart(roomId, partialResponse))
+                .delayElements(java.time.Duration.ofSeconds(10))
+                .doOnComplete(()->saveLLMResponse(roomId))
                 .subscribeOn(Schedulers.boundedElastic());
     }
-
     private Mono<InterviewMessage> saveHumanMessage(Long roomId, String message) {
         return messageRepository.save(InterviewMessage.createByMember(roomId, message))
                 .subscribeOn(Schedulers.boundedElastic());
     }
 
-    private void saveLLMResponse(Long roomId, List<String> responseBuffer) {
-        String fullResponse = String.join("", responseBuffer);
-        log.info("💬 전체 응답 완료: {}", fullResponse);
-        messageRepository.save(InterviewMessage.createByLLM(roomId, fullResponse))
+    private void appendLLMResponsePart(Long roomId, String partialBuffer) {
+        String redisKey = String.format("interview:%d:partial_response", roomId);
+        redisService.appendToString(redisKey, partialBuffer)
+                .subscribeOn(Schedulers.boundedElastic())
+                .doOnSuccess(length -> log.debug("Room {}: Appended '{}', current length: {}", roomId, partialBuffer, length))
+                .doOnError(error -> log.error("Room {}: Error appending '{}': {}", roomId, partialBuffer, error.getMessage()))
+                .subscribe();
+    }
+
+    private void saveLLMResponse(Long roomId) {
+
+        String redisKey = String.format("interview:%d:partial_response", roomId);
+        redisService.get(redisKey)
+                .flatMap(fullResponse -> {
+                    log.info("💬 스트림 완료 - 전체 응답 저장 시도: {}", fullResponse);
+                    return messageRepository.save(InterviewMessage.createByLLM(roomId, fullResponse))
+                            .then(redisService.delete(redisKey));
+                })
                 .subscribeOn(Schedulers.boundedElastic())
                 .subscribe();
+    }
+
+    public Mono<String> retrieveMessageHistory(Long roomId) {
+        String redisKey = String.format("interview:%d:partial_response", roomId);
+        return redisService.get(redisKey);
+    }
+
+    public Mono<List<InterviewSession>> retrieveInterviewRoom(Long memberId) {
+        return roomRepository.findByIntervieweeId(memberId)
+                .collectList()
+                .switchIfEmpty(Mono.error(new IllegalArgumentException("면접방이 존재하지 않습니다.")));
     }
 }
