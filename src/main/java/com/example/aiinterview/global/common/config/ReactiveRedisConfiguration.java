8e2bd9dc0a22a926c4531a87a98da6d473c23df0
KNU-K <knukang334@gmail.com>
2025-04-26 17:25:24 +0900

    feat: add ReactiveRedisConfiguration for reactive Redis support

---
+++ src/main/java/com/example/aiinterview/global/common/config/ReactiveRedisConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.aiinterview.global.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class ReactiveRedisConfiguration {
+
+    @Bean
+    public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+}
