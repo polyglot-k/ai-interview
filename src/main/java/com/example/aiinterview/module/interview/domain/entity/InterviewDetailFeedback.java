a4a22f3e95de1d0f47c4f95afe260b355862aab7
KNU-K <knukang334@gmail.com>
2025-05-05 00:39:04 +0900

    feat : InterviewDetailFeedback 테이블 매핑 interview 답변 1개 당 1개의 Feedback 이 생성 됨

---
+++ src/main/java/com/example/aiinterview/module/interview/domain/entity/InterviewDetailFeedback.java
@@ -0,0 +1,31 @@
+package com.example.aiinterview.module.interview.domain.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+@Table(name="interview_detail_feedback")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class InterviewDetailFeedback {
+    @Id
+    @Column("id")
+    private Long id;
+
+    @Column("feedback_text")
+    private String feedback;
+
+    @Column("score")
+    private Integer score;
+
+    @Column("created_at")
+    private LocalDateTime createdAt;
+
+    @Column("im_id")
+    private Long interviewMessageId;
+}
