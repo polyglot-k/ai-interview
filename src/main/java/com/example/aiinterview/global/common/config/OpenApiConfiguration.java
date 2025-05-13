233333291a03640311ceacbcd8af2c4aba4080ce
KNU-K <knukang334@gmail.com>
2025-05-13 20:10:11 +0900

    config : OpenAPI 내에 토큰 주입 가능 하도록 구성

---
+++ src/main/java/com/example/aiinterview/global/common/config/OpenApiConfiguration.java
@@ -0,0 +1,30 @@
+package com.example.aiinterview.global.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("WebFlux API")
+                        .version("v1")
+                        .description("API Documentation with JWT"))
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth",
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
