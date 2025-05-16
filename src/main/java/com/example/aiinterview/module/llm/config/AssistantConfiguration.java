6ac62190edb3b785be82004d5c6a213b51ac58d5
KNU-K <knukang334@gmail.com>
2025-05-16 17:15:55 +0900

    config : R2DBC기반 Memory 로 구성 후, 이를 싱글톤 형태로 주입 받도록 함.

--- src/main/java/com/example/aiinterview/module/llm/config/AssistantConfiguration.java
+++ src/main/java/com/example/aiinterview/module/llm/config/AssistantConfiguration.java
@@ -3,19 +3,22 @@ package com.example.aiinterview.module.llm.config;
 import com.example.aiinterview.module.llm.interviewer.assistant.BackendAssistant;
 import com.example.aiinterview.module.llm.interviewer.assistant.FrontendAssistant;
 import com.example.aiinterview.module.llm.interviewer.assistant.ReactiveAssistant;
-import com.example.aiinterview.module.llm.interviewer.memory.PersistentChatMemoryStore;
+import com.example.aiinterview.module.llm.interviewer.memory.R2dbcChatMemoryStore;
 import com.example.aiinterview.module.llm.interviewer.prompt.LLMPromptType;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.StreamingChatLanguageModel;
 import dev.langchain4j.service.AiServices;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Map;
 
 @Configuration
+@RequiredArgsConstructor
 public class AssistantConfiguration {
+    private final R2dbcChatMemoryStore chatMemoryStore;
     @Bean
     public BackendAssistant backendAssistant(StreamingChatLanguageModel model){
         return AiServices.builder(BackendAssistant.class)
@@ -40,11 +43,11 @@ public class AssistantConfiguration {
                     LLMPromptType.FRONTEND, frontendAssistantAdapter
                 );
     }
-    // Bean 으로 등록하면 안됨. memoryId 마다 memory 가 만들어져야하기 때문에 SingleTon 관리 X
+
     public ChatMemory memory(){
         return MessageWindowChatMemory.builder()
                 .maxMessages(10)
-                .chatMemoryStore(new PersistentChatMemoryStore())
+                .chatMemoryStore(chatMemoryStore)
                 .build();
     }
 }
