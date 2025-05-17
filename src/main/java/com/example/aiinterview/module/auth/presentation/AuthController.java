b0e79eb362528f7902f0f24ac01d851cf3cd1a76
KNU-K <knukang334@gmail.com>
2025-05-17 18:58:09 +0900

    feat: AuthController에 Swagger 주석 추가 및 로그 기록 기능 개선

--- src/main/java/com/example/aiinterview/module/auth/presentation/AuthController.java
+++ src/main/java/com/example/aiinterview/module/auth/presentation/AuthController.java
@@ -3,7 +3,10 @@ package com.example.aiinterview.module.auth.presentation;
 import com.example.aiinterview.module.auth.application.dto.LoginRequest;
 import com.example.aiinterview.module.auth.application.dto.LoginResponse;
 import com.example.aiinterview.module.auth.application.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,14 +16,19 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@Tag(name="AUTH API", description = "인증 관련 기능을 제공하는 API 입니다.")
+@Slf4j
 public class AuthController {
     private final AuthService authService;
     @PostMapping("/login")
+    @Operation(summary = "로그인 요청", description = "이메일과 비밀번호를 통한 로그인을 진행한다.")
     public Mono<LoginResponse> signIn(@RequestBody LoginRequest request){
+        log.info(request.toString());
         return authService.authenticate(request);
     }
 
     @PostMapping("/refresh")
+    @Operation(summary = "토큰 재발급", description = "미구현")
     public Mono<Void> refresh(){
         return Mono.empty();
     }
