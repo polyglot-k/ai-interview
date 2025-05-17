3f081815b7a56e34046f992f5221fcc1b6d3e85c
KNU-K <knukang334@gmail.com>
2025-05-17 18:59:24 +0900

    feat: 인터뷰 세션 관련 메서드에 Swagger 주석 추가 및 메서드 이름 변경

--- src/main/java/com/example/aiinterview/module/interview/presentation/InterviewController.java
+++ src/main/java/com/example/aiinterview/module/interview/presentation/InterviewController.java
@@ -1,11 +1,15 @@
 package com.example.aiinterview.module.interview.presentation;
 
+import com.example.aiinterview.global.sse.SseMapping;
 import com.example.aiinterview.module.interview.application.InterviewSessionService;
 import com.example.aiinterview.module.interview.application.dto.SseResponse;
 import com.example.aiinterview.module.interview.domain.entity.InterviewMessage;
 import com.example.aiinterview.module.interview.domain.entity.InterviewSession;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -14,48 +18,66 @@ import java.time.Duration;
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequestMapping("/api/v1/interviews")
 @RequiredArgsConstructor
+@Tag(name = "INTERVIEW API", description = "인터뷰 관련 기능을 제공하는 API입니다.")
 public class InterviewController {
     private final InterviewSessionService applicationService;
 
     @GetMapping()
-    public Mono<List<InterviewSession>> retrieveInterviewRoom(@RequestAttribute("userId") Long memberId){
-        return applicationService.retrieveInterviewRoom(memberId);
+    @Operation(summary = "인터뷰 세션 조회(토큰 필요)", description = "토큰을 포함해야 하며, 인터뷰 세션(방) 을 조회합니다.")
+    public Mono<List<InterviewSession>> retrieveInterviewSession(@RequestAttribute("userId") Long memberId){
+        return applicationService.retrieveInterviewSession(memberId);
     }
+
     @PostMapping()
-    public Mono<InterviewSession> createRoom(@RequestAttribute("userId") Long userId) {
-        return applicationService.createRoom(userId);
+    @Operation(summary = "인터뷰 세션 생성(토큰 필요)", description = "토큰을 포함해야 하며, 인터뷰 세션(방) 을 생성합니다.")
+    public Mono<InterviewSession> createSession(@RequestAttribute("userId") Long memberId) {
+        return applicationService.createRoom(memberId);
+    }
+
+    @PostMapping("/{sessionId}/complete")
+    @Operation(summary = "인터뷰 세션을 종료", description = "토큰을 포함해야하며, 인터뷰 세션을 종료합니다.")
+    public Mono<Void> completeSession(
+            @PathVariable("sessionId") Long sessionId
+    ){
+        return applicationService.completeInterview(sessionId);
     }
 
     @GetMapping("/{sessionId}/messages")
-    public Mono<List<InterviewMessage>> retrieveMessage(@PathVariable Long sessionId,
+    @Operation(summary = "인터뷰 세션의 모든 채팅 기록 조회(토큰 필요)", description = "토큰을 포함해야 하며, 인터뷰 세션(방) 내부의 메시지 정보를 조회합니다.")
+    public Mono<List<InterviewMessage>> retrieveMessage(@Parameter(description = "세션 고유 ID") @PathVariable Long sessionId,
                                                         @RequestAttribute("userId") Long memberId) {
         return applicationService.retrieveMessage(sessionId, memberId);
     }
-
     /**
      * 해당 유저가 속한 방 아니면 접근 못하게
      * @param sessionId
      * @param message
      * @return
      */
-    @GetMapping(value = "/{sessionId}/messages/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<SseResponse> sendMessage(@PathVariable Long sessionId,
+    @SseMapping("/{sessionId}/messages/stream")
+    @Operation(summary = "AI 스트리밍 진행(토큰 필요)", description = "토큰이 쿼리 스트링으로 포함되어야함. (?token=value) 의 형식으로, 이를 통해서 실시간 스트리밍을 진행")
+    public Flux<SseResponse> sendMessage(@Parameter(description = "세션 고유 ID") @PathVariable Long sessionId,
                                          @RequestAttribute("userId") Long userId,
-                                         @RequestParam("message") String message) {
-        Flux<SseResponse> messageFlux = applicationService.sendMessageAndStreamingLLM(sessionId, userId, message);
+//                                         @RequestHeader("Last-Event-ID") String lastEvnetId,
+                                         @Parameter(description = "메시지") @RequestParam("message") String message) {
+        Flux<SseResponse> messageFlux = applicationService.processMessageAndStreamingLLM(sessionId, userId, message).share();
         Flux<SseResponse> heartbeatFlux = generateHeartbeatPing().takeUntilOther(messageFlux);
         return Flux.merge(messageFlux, heartbeatFlux)
                 .concatWith(Mono.just(SseResponse.complete()));
     }
 
-
+    @Deprecated
     @GetMapping("/{sessionId}/buffer")
-    public Mono<String> retrieveMessageHistory(@PathVariable Long sessionId,
+    @Operation(summary = "deprecated", description = "AI 스트리밍 진행 시 데이터를 유실해서, 받아오지 못할 때, buffer 의 데이터를 가지고 오면 됨")
+    public Mono<String> retrieveMessageHistory(@Parameter(description = "세션 고유 ID") @PathVariable Long sessionId,
                                                @RequestAttribute("userId") Long memberId){
         return applicationService.retrieveMessageBuffer(sessionId);
     }
+
+
     private Flux<SseResponse> generateHeartbeatPing(){
         return Flux.interval(Duration.ofSeconds(1))
                 .map(tick -> SseResponse.heartbeat());
