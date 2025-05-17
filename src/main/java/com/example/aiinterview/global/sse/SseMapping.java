f28095bbdd8e7de461dd553abb9a6b92f993e69b
KNU-K <knukang334@gmail.com>
2025-05-17 18:55:11 +0900

    feat : SseMapping 어노테이션 추가 및 SSE 요청 처리 기능 구현

---
+++ src/main/java/com/example/aiinterview/global/sse/SseMapping.java
@@ -0,0 +1,24 @@
+package com.example.aiinterview.global.sse;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@RequestMapping(
+        method = RequestMethod.GET,
+        produces = MediaType.TEXT_EVENT_STREAM_VALUE
+)
+public @interface SseMapping {
+
+    @AliasFor(annotation = RequestMapping.class, attribute = "value")
+    String[] value() default {};
+
+    @AliasFor(annotation = RequestMapping.class, attribute = "path")
+    String[] path() default {};
+}
