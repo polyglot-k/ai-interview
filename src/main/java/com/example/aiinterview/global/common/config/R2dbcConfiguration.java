7dce2df8a0a8daf33a1da5ae35dd3953ad8dcbfd
KNU-K <knukang334@gmail.com>
2025-05-17 18:54:02 +0900

    refactor : R2DBC 설정에 트랜잭션 관리 추가

--- src/main/java/com/example/aiinterview/global/common/config/R2dbcConfiguration.java
+++ src/main/java/com/example/aiinterview/global/common/config/R2dbcConfiguration.java
@@ -4,16 +4,21 @@ import com.example.aiinterview.module.interview.infrastructure.converter.Intervi
 import com.example.aiinterview.module.interview.infrastructure.converter.InterviewSenderWriteConverter;
 import com.example.aiinterview.module.interview.infrastructure.converter.InterviewSessionStatusReadConverter;
 import com.example.aiinterview.module.interview.infrastructure.converter.InterviewSessionStatusWriteConverter;
+import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.r2dbc.convert.R2dbcCustomConversions;
 import org.springframework.data.r2dbc.dialect.H2Dialect;
 import org.springframework.data.r2dbc.dialect.R2dbcDialect;
+import org.springframework.r2dbc.connection.R2dbcTransactionManager;
+import org.springframework.transaction.ReactiveTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.Arrays;
 import java.util.List;
 @Configuration
+@EnableTransactionManagement
 public class R2dbcConfiguration {
     @Bean
     public R2dbcCustomConversions r2dbcCustomConversions(R2dbcDialect dialect) {
@@ -29,5 +34,10 @@ public class R2dbcConfiguration {
     @Bean
     public R2dbcDialect dialect() {
         return H2Dialect.INSTANCE;
+    }
+
+    @Bean
+    public ReactiveTransactionManager transactionManager(ConnectionFactory connectionFactory){
+        return new R2dbcTransactionManager(connectionFactory);
     }
 }
