621d3a6bb463aa80c4df4b421a91875fdc3a4386
KNU-K <knukang334@gmail.com>
2025-05-18 16:32:30 +0900

    feat: InterviewAnalyzer 클래스 추가 및 분석 기능 구현

--- src/main/java/com/example/aiinterview/module/llm/analysis/InterviewAnalyzer.java
+++ src/main/java/com/example/aiinterview/module/llm/analysis/InterviewAnalyzer.java
@@ -1,9 +1,10 @@
 package com.example.aiinterview.module.llm.analysis;
 
-import com.example.aiinterview.module.interview.infrastructure.repository.InterviewMessageRepository;
 import com.example.aiinterview.module.llm.analysis.dto.InterviewSessionResult;
+import com.example.aiinterview.module.llm.analysis.dto.PartialEvaluation;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.chat.request.ChatRequest;
@@ -18,34 +19,24 @@ import reactor.core.scheduler.Schedulers;
 @Component
 @Slf4j
 @RequiredArgsConstructor
-public class InterviewSessionAnalyzer {
+public class InterviewAnalyzer implements Analyzer<InterviewSessionResult>{
 
     private final ChatLanguageModel chatModel;
-    private final InterviewMessageRepository repository;
     private final ResponseFormat responseFormat;
     private final ObjectMapper objectMapper;
 
-    public Mono<InterviewSessionResult> analyze(Long interviewSessionId) {
-        return loadInterviewSessionMessage(interviewSessionId)
-                .flatMap(this::analyzeMessage);  // 전체를 하나로 묶는다
-    }
-
-    private Mono<InterviewSessionResult> analyzeMessage(String message) {
+    @Override
+    public Mono<InterviewSessionResult> analyze(String message) {
         return Mono.fromCallable(() -> {
                     log.info(message);
                     UserMessage userMessage = UserMessage.from(message);
                     ChatRequest chatRequest = createChatRequest(userMessage);
                     ChatResponse chatResponse = chatModel.chat(chatRequest);
                     String output = chatResponse.aiMessage().text();
+                    log.error("output : {}", output);
                     return deserializeResult(output);
                 })
-                .subscribeOn(Schedulers.boundedElastic()); // CPU or Blocking 작업 분리
-    }
-
-    private Mono<String> loadInterviewSessionMessage(Long interviewSessionId) {
-        return repository.findBySessionId(interviewSessionId)
-                .map(msg ->"[ id : "+ msg.getId()+ "]" + msg.getSender() + ": " + msg.getMessage())
-                .reduce((a, b) -> a + "\n" + b);
+                .subscribeOn(Schedulers.boundedElastic());
     }
 
     private ChatRequest createChatRequest(UserMessage userMessage) {
