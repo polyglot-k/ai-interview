36103c7178105e1e6721f1ebb7baeb43875901c7
KNU-K <knukang334@gmail.com>
2025-05-24 03:12:51 +0900

    feat: InterviewMessageRepository 인터페이스를 R2dbcRepository에서 ReactiveCrudRepository로 변경 및 세션 ID와 발신자에 따른 메시지 수 카운트 기능 추가

--- src/main/java/com/example/aiinterview/module/interview/domain/repository/InterviewMessageRepository.java
+++ src/main/java/com/example/aiinterview/module/interview/domain/repository/InterviewMessageRepository.java
@@ -1,12 +1,13 @@
 package com.example.aiinterview.module.interview.domain.repository;
 
 import com.example.aiinterview.module.interview.domain.entity.InterviewMessage;
-import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import com.example.aiinterview.module.interview.domain.vo.InterviewSender;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-public interface InterviewMessageRepository extends R2dbcRepository<InterviewMessage, Long> {
+public interface InterviewMessageRepository extends ReactiveCrudRepository<InterviewMessage, Long>,InterviewMessageCompositeRepositoryCustom{
 
+    Mono<Long> countBySessionIdAndSender(Long sessionId, InterviewSender Sender);
     Flux<InterviewMessage> findBySessionId(Long sessionId);
-    Mono<Long> countBySessionIdAndSender(Long sessionId, String Sender);
 }
