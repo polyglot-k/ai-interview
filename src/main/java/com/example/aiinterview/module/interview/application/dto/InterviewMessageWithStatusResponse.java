ac2989f76fa60f5cd86d1228c7d84aaa8ebba309
KNU-K <knukang334@gmail.com>
2025-06-03 05:06:25 +0900

    refactor: InterviewMessageWithStatusResponse 클래스에서 메시지 필드 타입 변경 및 정적 팩토리 메서드 추가

--- src/main/java/com/example/aiinterview/module/interview/application/dto/InterviewMessageWithStatusResponse.java
+++ src/main/java/com/example/aiinterview/module/interview/application/dto/InterviewMessageWithStatusResponse.java
@@ -1,6 +1,5 @@
 package com.example.aiinterview.module.interview.application.dto;
 
-import com.example.aiinterview.module.interview.domain.entity.InterviewMessage;
 import com.example.aiinterview.module.interview.domain.vo.InterviewMessageWithStatus;
 import com.example.aiinterview.module.interview.domain.vo.InterviewSender;
 import com.example.aiinterview.module.interview.domain.vo.InterviewSessionStatus;
@@ -16,18 +15,29 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class InterviewMessageWithStatusResponse {
-    List<InterviewMessage> messages;
-    InterviewSessionStatus status;
+    private List<InterviewMessageResponse> messages;
+    private InterviewSessionStatus status;
+    public static InterviewMessageWithStatusResponse of(List<InterviewMessageResponse> messages, InterviewSessionStatus status) {
+        return new InterviewMessageWithStatusResponse(messages, status);
+    }
 
 
     @Data
     @AllArgsConstructor
     @NoArgsConstructor
-    static public class InterviewMessage {
+    static public class InterviewMessageResponse {
         private Long id;
         private InterviewSender sender;
         private String message;
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
         private LocalDateTime createdAt;
+
+        public static InterviewMessageResponse ofLLM(Long id, String message, LocalDateTime createdAt) {
+            return new InterviewMessageResponse(id, InterviewSender.LLM, message,createdAt);
+        }
+
+        public static InterviewMessageResponse ofUser(Long id, String message, LocalDateTime createdAt) {
+            return new InterviewMessageResponse(id, InterviewSender.USER, message,createdAt);
+        }
     }
 }
