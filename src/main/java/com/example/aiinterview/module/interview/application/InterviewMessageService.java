ec07890acf3442f8992b686edd805754256193df
KNU-K <knukang334@gmail.com>
2025-06-03 05:06:01 +0900

    feat: InterviewMessageService에서 메시지 저장 및 조회 로직 개선, 사용자 메시지 처리 추가

--- src/main/java/com/example/aiinterview/module/interview/application/InterviewMessageService.java
+++ src/main/java/com/example/aiinterview/module/interview/application/InterviewMessageService.java
@@ -1,19 +1,21 @@
 package com.example.aiinterview.module.interview.application;
 
+import com.example.aiinterview.module.interview.application.dto.InterviewMessageWithStatusResponse;
 import com.example.aiinterview.module.interview.domain.entity.InterviewMessage;
-import com.example.aiinterview.module.interview.domain.repository.InterviewMessageCompositeRepositoryCustom;
 import com.example.aiinterview.module.interview.domain.repository.InterviewMessageRepository;
-import com.example.aiinterview.module.interview.domain.vo.InterviewMessageWithStatus;
-import com.example.aiinterview.module.interview.domain.vo.InterviewSender;
 import com.example.aiinterview.module.interview.exception.InterviewSessionNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import static com.example.aiinterview.module.interview.application.dto.InterviewMessageWithStatusResponse.InterviewMessageResponse;
 
 @Service
 @RequiredArgsConstructor
@@ -21,20 +23,49 @@ public class InterviewMessageService {
     private final InterviewMessageRepository messageRepository;
 
     @Transactional(propagation = Propagation.REQUIRED)
-    public Mono<InterviewMessage> saveMessage(Long sessionId, String message, InterviewSender sender) {
-        InterviewMessage interviewMessage = switch (sender){
-            case LLM -> InterviewMessage.createByLLM(sessionId, message);
-            case USER -> InterviewMessage.createByMember(sessionId, message);
-        };
+    public Mono<InterviewMessage> saveQuestionByLLM(Long sessionId, String message) {
+        InterviewMessage interviewMessage = InterviewMessage.createByLLM(sessionId, message);
 
         return messageRepository.save(interviewMessage)
                 .subscribeOn(Schedulers.boundedElastic());
     }
 
+    @Transactional(propagation = Propagation.REQUIRED)
+    public Mono<Void> saveAnswerByUser(Long sessionId, String message) {
+        return messageRepository.saveUserMessage(sessionId, message, LocalDateTime.now())
+                .subscribeOn(Schedulers.boundedElastic())
+                .then();
+    }
     @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
-    public Mono<InterviewMessageWithStatus> retrieveMessageWithStatus(Long sessionId) {
+    public Mono<InterviewMessageWithStatusResponse> retrieveMessageWithStatus(Long sessionId) {
         return messageRepository.retrieveMessageWithStatusBySessionId(sessionId)
-                .switchIfEmpty(Mono.error(InterviewSessionNotFoundException::new));
+                .switchIfEmpty(Mono.error(InterviewSessionNotFoundException::new))
+                .flatMap(interviewMessageWithStatus -> {
+                    Flux<InterviewMessageResponse> messageResponsesFlux = Flux.fromIterable(interviewMessageWithStatus.getInterviewMessage())
+                            .flatMap(interviewMessageV2 -> {
+                                Mono<InterviewMessageResponse> llmResponse = Mono.just(
+                                        InterviewMessageResponse.ofLLM(
+                                                interviewMessageV2.getId(),
+                                                interviewMessageV2.getLlmMessage(),
+                                                interviewMessageV2.getLlmCreatedAt()
+                                        )
+                                );
+                                Mono<InterviewMessageResponse> userResponse = Mono.empty();
+                                if (interviewMessageV2.getUserMessage() != null) {
+                                    userResponse = Mono.just(
+                                            InterviewMessageResponse.ofUser(
+                                                    interviewMessageV2.getId(),
+                                                    interviewMessageV2.getUserMessage(),
+                                                    interviewMessageV2.getUserCreatedAt()
+                                            )
+                                    );
+                                }
+                                return Flux.concat(llmResponse, userResponse);
+                            });
+
+                    return messageResponsesFlux.collectList()
+                            .map(responses -> InterviewMessageWithStatusResponse.of(responses, interviewMessageWithStatus.getStatus()));
+                });
     }
     @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
     public Mono<List<InterviewMessage>> retrieveMessage(Long sessionId) {
@@ -44,10 +75,11 @@ public class InterviewMessageService {
     }
     @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
     public Mono<Long> retrieveCount(Long sessionId) {
-        return messageRepository.countBySessionIdAndSender(sessionId, InterviewSender.USER);
+        return messageRepository.countBySessionId(sessionId);
     }
 
     public Mono<Void> deleteLastMessage(Long sessionId) {
         return messageRepository.deleteById(sessionId);
     }
+
 }
