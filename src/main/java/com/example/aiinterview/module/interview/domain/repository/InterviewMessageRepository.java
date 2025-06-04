57e04d45c5c5ca195840cb290537ed15bc18018c
KNU-K <knukang334@gmail.com>
2025-06-04 19:43:44 +0900

    refactor: InterviewMessageRepository에서 쿼리 형식 개선 및 불필요한 코드 제거

--- src/main/java/com/example/aiinterview/module/interview/domain/repository/InterviewMessageRepository.java
+++ src/main/java/com/example/aiinterview/module/interview/domain/repository/InterviewMessageRepository.java
@@ -10,17 +10,39 @@ import reactor.core.publisher.Mono;
 import java.time.LocalDateTime;
 
 public interface InterviewMessageRepository extends ReactiveCrudRepository<InterviewMessage, Long>, InterviewMessageCompositeRepositoryCustom {
-
+    //TODO: UPDATE 쿼리 쿼리 튜닝 대상
     @Modifying
-    @Query("UPDATE interview_message_v2 " +
-            "SET user_content = :userContent, user_created_at = :userCreatedAt " +
-            "WHERE i_id = :sessionId " +
-            "ORDER BY llm_created_at DESC " +
-            "LIMIT 1")
+    @Query("""
+    UPDATE interview_message
+    SET user_content = :userContent, user_created_at = :userCreatedAt
+    WHERE i_id = :sessionId
+    ORDER BY llm_created_at DESC
+    LIMIT 1
+            """)
 
     Mono<Void> saveUserMessage(Long sessionId, String userContent, LocalDateTime userCreatedAt);
 
     Mono<Long> countBySessionId(Long sessionId);
+
     Flux<InterviewMessage> findBySessionId(Long sessionId);
 
+    @Query("""
+     SELECT
+        m.id ,
+        m.user_content
+    FROM
+    interview_message m
+    where m.id = :messageId;
+    """)
+    Mono<InterviewMessage> findUserContent(Long messageId);
+
+    @Query("""
+     SELECT
+        m.id ,
+        m.llm_content
+    FROM
+    interview_message m
+    where m.id = :messageId;
+    """)
+    Mono<InterviewMessage> findLlmContent(Long messageId);
 }
