642d20889a972ba3a9f1efba85097885363924ca
KNU-K <knukang334@gmail.com>
2025-06-04 19:43:58 +0900

    feat: InterviewResultSummaryRepository에 findByInterviewSessionId 메서드 추가

--- src/main/java/com/example/aiinterview/module/interview/domain/repository/InterviewResultSummaryRepository.java
+++ src/main/java/com/example/aiinterview/module/interview/domain/repository/InterviewResultSummaryRepository.java
@@ -2,6 +2,9 @@ package com.example.aiinterview.module.interview.domain.repository;
 
 import com.example.aiinterview.module.interview.domain.entity.InterviewResultSummary;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Mono;
 
 public interface InterviewResultSummaryRepository  extends R2dbcRepository<InterviewResultSummary, Long> {
+
+    Mono<InterviewResultSummary> findByInterviewSessionId(Long interviewSessionId);
 }
