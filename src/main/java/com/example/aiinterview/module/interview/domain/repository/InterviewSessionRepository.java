6da5c777ad7825006821ad0556dfc32ae571871c
KNU-K <knukang334@gmail.com>
2025-06-06 21:07:18 +0900

    feat: InterviewSessionRepository에 강제 인덱스를 사용하는 세션 조회 메서드 추가(슬로우 쿼리 완화) Primary 를 옵티마이저가 계속 선택하여, 슬로우 쿼리 발생하는 현상을 covering index 를 강제하여 완화화

--- src/main/java/com/example/aiinterview/module/interview/domain/repository/InterviewSessionRepository.java
+++ src/main/java/com/example/aiinterview/module/interview/domain/repository/InterviewSessionRepository.java
@@ -1,6 +1,7 @@
 package com.example.aiinterview.module.interview.domain.repository;
 
 import com.example.aiinterview.module.interview.domain.entity.InterviewSession;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -9,5 +10,15 @@ public interface InterviewSessionRepository extends R2dbcRepository<InterviewSes
     // 면접방 ID로 조회 (Mono는 비동기 결과를 나타냄)
     Mono<InterviewSession> findById(Long id);
     Flux<InterviewSession> findByIntervieweeId(Long intervieweeId);
+
+    @Query("""
+        SELECT 
+        id,
+        status, 
+        u_id
+        FROM interview_session force index(interview_session_covering_idx)
+        where id=:sessionId;
+    """)
+    Mono<InterviewSession> findByIdForcedCoveringIndex(Long sessionId);
 }
 
