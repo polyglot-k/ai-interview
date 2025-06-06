f2d69b99b4c69140e9b28a2a4719845baf4adb64
KNU-K <knukang334@gmail.com>
2025-06-06 21:03:39 +0900

    feat: ReactiveRedisConfiguration에 ObjectMapper를 사용한 RedisTemplate 설정 추가

--- src/main/java/com/example/aiinterview/global/common/config/ReactiveRedisConfiguration.java
+++ src/main/java/com/example/aiinterview/global/common/config/ReactiveRedisConfiguration.java
@@ -1,8 +1,36 @@
 package com.example.aiinterview.global.common.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@RequiredArgsConstructor
 public class ReactiveRedisConfiguration {
+    private final ObjectMapper mapper;
+    @Bean
+    public ReactiveRedisTemplate<String, Object> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
+        StringRedisSerializer keySerializer = new StringRedisSerializer();
+
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        Jackson2JsonRedisSerializer<Object> valueSerializer = new Jackson2JsonRedisSerializer<>(mapper, Object.class);
+
+        RedisSerializationContext.RedisSerializationContextBuilder<String, Object> builder =
+                RedisSerializationContext.newSerializationContext(keySerializer);
+
+        RedisSerializationContext<String, Object> context = builder.value(valueSerializer).build();
+
+        return new ReactiveRedisTemplate<>(factory, context);
+    }
 
 }
